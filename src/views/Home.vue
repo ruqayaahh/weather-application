e780f0646f600add290011e55fd070adc9bff9a5
ruqayaahh <aderinolaruqayaah@gmail.com>
2021-03-02 11:04:25 +0100

    initial commit w/0 css

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,69 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld
+      :wel="wel"
+      :msg="msg"
+      @getReport="weatherData"
+      :info="info"
+      :city="city"
+      :temp="temp"
+      :weather="weather"
+      :description="description"
+      :showImage="showImage"
+      :hot="hot"
+    />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  data() {
+    return {
+      wel: "Welcome to Our Weather Report Platform",
+      msg: "Check weather report of your current location",
+      info: {},
+      city: "",
+      kelvin: 0,
+      temp: 0,
+      weather: "",
+      description: "",
+      showImage: false,
+      hot: false,
+    };
+  },
+  methods: {
+    weatherData(cityname) {
+      this.showImage = false;
+      const api = "https://api.openweathermap.org/data/2.5/weather?q=";
+      let city = cityname;
+      const apiKey = "&appid=9678383e74cdb44e52cba56dcb3621d0";
+      let apiURL = api + city + apiKey;
+
+      fetch(apiURL)
+        .then((response) => response.json())
+        .then((data) => {
+          this.info = data;
+          this.city = city;
+          this.kelvin = this.info.main.temp;
+          this.temp = Math.round(this.kelvin - 273);
+          this.weather = this.info.weather[0].main;
+          this.description = this.info.weather[0].description;
+          if (this.temp < 23) {
+            return (this.hot = false);
+          } else {
+            return (this.hot = true);
+          }
+        })
+        .catch((error) => alert(error))
+        .finally(() => (this.showImage = true));
+    },
+  },
+};
 </script>
